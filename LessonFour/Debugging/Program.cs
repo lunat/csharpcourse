d485af37b22ac8b5342b2c362768ed8f2bcfb686
Luca <lunat38@gmail.com>
2022-05-25 13:04:47 +0200

    Added Unit Tests.

---
+++ LessonFour/Debugging/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+WriteLine($"{a} + {b} = {answer}");
+
+
+static double Add(double a1, double b1)
+{
+    return a1 * b1;
+}
---
+++ LessonFour/Logging/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using static System.Console;
+using Microsoft.Extensions.Configuration;
+
+Trace.Listeners.Add(new TextWriterTraceListener(
+    File.CreateText(Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+
+Trace.AutoFlush = true;
+
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json",
+        optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+TraceSwitch ts = new(
+    displayName: "LogSwitch",
+    description: "This switch is set via a JSON config.");
+
+configuration.GetSection("LogSwitch").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "Trace Error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace Warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace Info");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace Verbose");
+
+
+//
+//Debug.WriteLine("Debug says I am watching");
+//Trace.WriteLine("Trace says I am watching");
